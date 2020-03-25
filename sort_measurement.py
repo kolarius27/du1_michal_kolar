4d2a5252213c46cd3eb0362744c5f1418e7493cc
Michal Kolář <michalkolar27@gmail.com>
2020-03-25 21:57:53 +0100

    2020-03-25_21:56 – implementing csv

--- sort_measurement.py
+++ sort_measurement.py
@@ -1,5 +1,8 @@
 import random
 import time
+import math
+import csv
+import string
 
 
 def random_list_num(n):
@@ -36,7 +39,7 @@ def insert_sort(xlist):
     return ylist
 
 
-def quick_sort(xlist, left, right):
+def q_sort(xlist, left, right):
     pivot = xlist[(left + right) // 2]
     leftb = left
     rightb = right
@@ -52,91 +55,92 @@ def quick_sort(xlist, left, right):
             left += 1
             right -= 1
     if leftb < right:
-        quick_sort(xlist, leftb, right)
+        q_sort(xlist, leftb, right)
     if rightb > left:
-        quick_sort(xlist, left, rightb)
+        q_sort(xlist, left, rightb)
 
 
-def q_sort(xlist):
-    quick_sort(xlist, 0, len(xlist)-1)
+def quick_sort(xlist):
+    q_sort(xlist, 0, len(xlist)-1)
     return xlist
 
 
 def int_list_generating(k):
     alist = random_list_num(4 ** k)
-    blist = alist
-    blist.sort()
-    clist = alist
-    for _ in range(len(alist) // 100):
-        i = random.randint(0, len(alist))
-        print(i)
-        j = random.randint(0, len(alist))
+    blist = quick_sort(alist[:])
+    clist = blist[:]
+    for _ in range(math.comb(len(clist), 2) // 100):
+        i = random.randint(0, len(clist)-1)
+        j = random.randint(0, len(clist)-1)
         while i == j:
-            j = random.randint(0, len(alist))
-        print(j)
+            j = random.randint(0, len(clist)-1)
         temp = clist[i]
         clist[i] = clist[j]
         clist[j] = temp
-    return alist, blist, clist
+    return [alist, blist, clist]
 
 
-def time_sort_measuring(k, xlist, function):
-    time_xlist_k = []
+def time_sort_measuring(k, xlist, function, reader1, writer1, field_name):
     if k < 7:
-        val = 100
+        for row in reader1:
+            if function is None:
+                before = time.time_ns()
+                xlist.sort()
+                after = time.time_ns()
+                interval = after - before
+            else:
+                before = time.time_ns()
+                xlist = function(xlist)
+                after = time.time_ns()
+                interval = after - before
+            print(row, field_name, interval)
+            writer1.writerow(next(reader1), {field_name: interval})
     else:
-        val = 10
-    for l in range(val):
-        if function is None:
-            before = time.time_ns()
-            xlist = xlist.sort()
-            interval = time.time_ns() - before
-        else:
-            before = time.time_ns()
-            xlist = function(xlist)
-            interval = time.time_ns() - before
-        time_xlist_k.append(interval)
-    return time_xlist_k
+        for row in reader1[:11]:
+            if function is None:
+                before = time.time_ns()
+                xlist.sort()
+                after = time.time_ns()
+                interval = after - before
+            else:
+                before = time.time_ns()
+                xlist = function(xlist)
+                after = time.time_ns()
+                interval = after - before
+            writer1.writerow(next(reader1), {field_name: interval})
 
 
-sort_full = []
-insert_sort_full = []
-select_sort_full = []
-quick_sort_full = []
+functions = [None, select_sort, quick_sort]
 
-full_list = [(None, sort_full), (insert_sort, insert_sort_full),
-             (select_sort, select_sort_full), (quick_sort, quick_sort_full)]
+with open('sort_alg.csv', 'w', newline='') as csvfile2:
+    with open('sort_alg.csv', 'r', newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
 
-for k in range(3, 11, 1):
-    a_list, b_list, c_list = int_list_generating(k)
-    print(a_list)
-    print(b_list)
-    print(c_list)
-    for function, array in full_list:
-        print(function, array)
-        sort_a_list_k = time_sort_measuring(k, a_list, function)
-        sort_b_list_k = time_sort_measuring(k, b_list, function)
-        sort_c_list_k = time_sort_measuring(k, c_list, function)
-        array.append(sort_a_list_k, sort_b_list_k, sort_c_list_k)
+        fieldnames_sort = ['iteration']
 
-print(full_list)
+        for k in range(3, 6, 1):
+            print(k)
+            abc_list = int_list_generating(k)
+            for fc in functions:
+                for i in range(3):
+                    if fc is None:
+                        fieldname = str(k) + "_sort_" + string.ascii_lowercase[i]
+                    else:
+                        fieldname = str(k) + "_" + fc.__name__ + "_" + string.ascii_lowercase[i]
+                    fieldnames_sort.append(fieldname)
 
+        writer = csv.DictWriter(csvfile2, fieldnames=fieldnames_sort)
 
+        for i in range(1, 101):
+            writer.writerow({'iteration': i})
 
+        reader = csv.DictReader(csvfile)
+        print(reader)
 
-
-
-
-
-
-
-
-print(alist)
-print(q_sort(alist))
-
-
-
-
-
-
-
+        index = 1
+        for k in range(3,6,1):
+            [a_list, b_list, c_list] = int_list_generating(k)
+            for fc in functions:
+                for _list in [a_list, b_list, c_list]:
+                    time_sort_measuring(k, _list, fc, reader, writer, fieldnames_sort[index])
+                    index += 1
